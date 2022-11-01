c0cba29edb1ab50f3103de4856dd8ada0c265deb
Yu-Hoto <yu.hoto.0220@gmail.com>
2022-11-01 17:57:30 +0900

    fix transition animation and refactor dispatchqueue.

--- CameraForcusExample/ContentView.swift
+++ CameraForcusExample/ContentView.swift
@@ -4,6 +4,7 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
 
@@ -37,39 +38,46 @@ private struct CameraModifier: ViewModifier {
     @State var camera: Camera
     @State var tapPositionState: CGPoint?
 
-    func body(content: Content) -> some View {
+    let publisher = PassthroughSubject<CGPoint, Never>()
 
-        if let position = tapPositionState {
-            content
-                .overlay(
-                    Rectangle()
-                        .stroke(Color.yellow, lineWidth: 2)
-                        .frame(width: 100, height: 100, alignment: .center)
-                        .position(x: position.x, y: position.y)
-                        .animation(nil)
-                        .transition(.scale)
-                        .onAppear { print("onAppear(transition)") }
-                        .onDisappear { print("onDisappear(transition)") }
-                )
-        } else {
-            content
-                .gesture(
-                    DragGesture(minimumDistance: 0, coordinateSpace: .global)
-                        .onEnded { value in
-                            print("tapped location(view): ", value.location)
-                            tapPositionState = value.location
-                            let point = camera.previewLayer?.captureDevicePointConverted(fromLayerPoint: value.location)
-                            print("tapped location(camera): ", point ?? .zero)
-                            camera.forcusAndExposure(point)
-
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                                tapPositionState = nil
-                            }
-                        }
-                )
-                .onAppear { print("onAppear(gesture)") }
-                .onDisappear { print("onDisappear(gesture)") }
+    @ViewBuilder
+    var sight: some View {
+        GeometryReader { geometry in
+            if let position = tapPositionState {
+                Rectangle()
+                    .stroke(Color.yellow, lineWidth: 2)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .transition(.opacity.combined(with: .scale(scale: 1.25, anchor: UnitPoint(x: position.x / geometry.size.width, y: position.y / geometry.size.height))))
+                    .position(x: position.x, y: position.y)
+            }
         }
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .overlay(sight)
+            .gesture(
+                DragGesture(minimumDistance: 0, coordinateSpace: .global)
+                    .onEnded { value in
+                        print("tapped location(view): ", value.location)
+                        publisher.send(value.location)
+                        let point = camera.previewLayer?.captureDevicePointConverted(fromLayerPoint: value.location)
+                        print("tapped location(camera): ", point ?? .zero)
+                        camera.forcusAndExposure(point)
+                    }
+            )
+            .onReceive(publisher) { value in
+                withAnimation {
+                    tapPositionState = value
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    if value == tapPositionState {
+                        withAnimation {
+                            tapPositionState = nil
+                        }
+                    }
+                }
+            }
     }
 }
 
